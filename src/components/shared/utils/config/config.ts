86940e8a967b256d510cfba670d364311e2c1bed
Iamsteve120 <125187687+Iamsteve120@users.noreply.github.com>
2026-03-30 21:36:39 +0300

    Update domain_app_ids for new production domain

--- src/components/shared/utils/config/config.ts
+++ src/components/shared/utils/config/config.ts
@@ -18,7 +18,7 @@ export const livechat_license_id = 12049137;
 export const livechat_client_id = '66aa088aad5a414484c1fd1fa8a5ace7';
 
 export const domain_app_ids = {
-    'bot.proedgetrade.site': APP_ID,
+    'bot.edgepro.site': APP_ID,
 };
 
 export const getCurrentProductionDomain = () =>
